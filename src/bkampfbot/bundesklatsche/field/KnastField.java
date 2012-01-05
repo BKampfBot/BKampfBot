b253a0504ed5fcde956607eba966254b41206f6b
georf <georf@georf.de>
2012-01-05 21:00:34 +0100

    Kanst implementiert

--- src/bkampfbot/bundesklatsche/field/KnastField.java
+++ src/bkampfbot/bundesklatsche/field/KnastField.java
@@ -1,6 +1,8 @@
 package bkampfbot.bundesklatsche.field;
 
+import json.JSONException;
 import json.JSONObject;
+import bkampfbot.modes.Bundesklatsche;
 import bkampfbot.output.Output;
 
 public class KnastField extends Field {
@@ -10,11 +12,39 @@ public class KnastField extends Field {
 	}
 
 	@Override
-	public boolean action() {
+	public boolean action() throws JSONException {
 
+		/*
+    "action": {
+        "cont": "knast",
+        "moveon": "0",
+        "raus": 0
+    },
+    
+    
+    "action": {
+        "cont": "knast",
+        "blackdice": 6,
+        "beute": null,
+        "raus": 1,
+        "moveon": "1"
+    },
+    */
+		
+		for (int i = 0; i < 10; i++) {
+			
+			JSONObject action = result.getJSONObject("action");
+			if (action.getInt("raus") == 1) {
+				return true;
+			}
+			
+			Bundesklatsche.rollTheDice();
+			
+		}
+		
 		// TODO Nicht implementiert
 
-		Output.printTabLn("Nicht implementiert: "
+		Output.printTabLn("Nicht funktioniert: "
 				+ this.getClass().getSimpleName(), Output.ERROR);
 		return false;
 	}
