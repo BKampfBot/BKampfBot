0c6a430841b00fdfaf354b4ff65edd03c025d06e
georf <georf@georf.de>
2012-01-08 23:27:44 +0100

    Ereignis-Feld fertig

--- src/bkampfbot/bundesklatsche/field/EreignisField.java
+++ src/bkampfbot/bundesklatsche/field/EreignisField.java
@@ -12,29 +12,23 @@ public class EreignisField extends Field {
 
 	@Override
 	public boolean action() throws JSONException {
-/*"action": {
-        "cont": "",
-        "text": "Auch wenn es Bogdan nicht gerne sieht: Du verkaufst Rubbellose zu Gunsten der Aktion Morgenwind. Deine Einnahmen erreichen bereits am ersten Tag:",
-        "bonus": "1",
-        "gold": 1702,
-        "klatschen": 5
-    },*/
+		/*
+		 * "action": { "cont": "", "text":
+		 * "Auch wenn es Bogdan nicht gerne sieht: Du verkaufst Rubbellose zu Gunsten der Aktion Morgenwind. Deine Einnahmen erreichen bereits am ersten Tag:"
+		 * , "bonus": "1", "gold": 1702, "klatschen": 5 },
+		 */
 		JSONObject action = result.getJSONObject("action");
-		
-		
-		Output.printClock("Ereignis - ", Output.INFO);
-		
-		if (action.getString("bonus").equals("1")) {
-			Output.println("gewonnen ("+action.getInt("gold")+" DM, "+action.getInt("klatschen")+" Klatschen)", Output.INFO);
-			return true;
-		} else {
-			
-			// TODO Nicht implementiert
 
-			Output.printTabLn("Nicht implementiert: "
-					+ this.getClass().getSimpleName(), Output.ERROR);
-			return false;
-			
+		Output.printClock("Ereignis - ", Output.INFO);
+
+		if (action.getString("bonus").equals("1")) {
+			Output.print("gewonnen", Output.INFO);
+		} else {
+			Output.print("verloren", Output.INFO);
 		}
+
+		Output.println(" " + action.getInt("gold") + " DM, "
+				+ action.getInt("klatschen") + " Klatschen)", Output.INFO);
+		return true;
 	}
 }
