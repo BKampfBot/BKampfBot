7c2f3913fabbd79e17660eed2ff9f34bc3f39378
georf <georf@georf.de>
2012-01-10 23:41:37 +0100

    Startfeld wirklich fertig

--- src/bkampfbot/bundesklatsche/field/StartField.java
+++ src/bkampfbot/bundesklatsche/field/StartField.java
@@ -2,7 +2,6 @@ package bkampfbot.bundesklatsche.field;
 
 import json.JSONException;
 import json.JSONObject;
-import bkampfbot.Utils;
 import bkampfbot.output.Output;
 
 public class StartField extends Field {
@@ -14,10 +13,6 @@ public class StartField extends Field {
 	@Override
 	public boolean action() throws JSONException {
 		Output.printClockLn("Startfeld", Output.INFO);
-		
-		if (result.getInt("payout") == 1) {
-			Utils.getString("bundesklatsche/get_data/2");
-		}
 		return true;
 	}
 
