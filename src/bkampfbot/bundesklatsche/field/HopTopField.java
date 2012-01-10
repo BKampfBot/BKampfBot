9bb3c46c55d730c5436b524a7e6e328f1d173582
georf <georf@georf.de>
2012-01-11 00:24:01 +0100

    HopTop eingebunden

--- src/bkampfbot/bundesklatsche/field/HopTopField.java
+++ src/bkampfbot/bundesklatsche/field/HopTopField.java
@@ -2,6 +2,7 @@ package bkampfbot.bundesklatsche.field;
 
 import json.JSONException;
 import json.JSONObject;
+import bkampfbot.Control;
 import bkampfbot.Utils;
 import bkampfbot.output.Output;
 
@@ -13,7 +14,7 @@ public class HopTopField extends Field {
 
 	@Override
 	public boolean action() throws JSONException {
-/*
+
 		// wieviel einsatz ist möglich? maximal bestimmt die bisherige beute?
 
 		// einsatz eingeben
@@ -25,23 +26,18 @@ public class HopTopField extends Field {
 		// rückgabe enthält srn
 		// {"status":"ok","srn":7590}
 
-		int code = Math.round(((set.getInt("srn") * 5 + 234) * 2 - 1104) / 5);
+		int code = Math.round(((set.getInt("srn") * 5 + 234) * 2 - 1104) / 5F);
 		// POST /bundesklatsche/fin_hoptop/1/15053 HTTP/1.1
 		// RundenVon(((SicherheitsCode * 5 + 234) * 2 - 1104) / 5)
-
+Control.sleep(150, Output.INFO);
 		try {
 			Utils.getJSON("bundesklatsche/fin_hoptop/1/" + code);
 			return true;
 		} catch (JSONException e) {
 			Output.printTabLn("Es gab einen Fehler bei HopTop", Output.ERROR);
 			return false;
-		}*/
+		}
 
-		// TODO Nicht implementiert
-
-		Output.printTabLn("Nicht implementiert: "
-				+ this.getClass().getSimpleName(), Output.ERROR);
-		return false;
 	}
 
 }
