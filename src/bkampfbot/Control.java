771ce9b632eb84ea0a6ebc08737a6362c6a7891c
georf <georf@georf.de>
2013-05-16 23:35:24 +0200

    Versionshinweise

--- src/bkampfbot/Control.java
+++ src/bkampfbot/Control.java
@@ -27,7 +27,7 @@ import bkampfbot.state.Config;
 import bkampfbot.state.Prevention;
 
 public class Control {
-	public final static String version = "1.3.8rc3";
+	public final static String version = "1.3.9";
 
 	public static Instance current = null;
 
